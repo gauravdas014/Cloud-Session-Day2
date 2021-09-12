2eb6fc8a611afe9297e8b67d3b69a873b446f6dc
Gaurav Das <gauravdas014@gmail.com>
2021-09-12 15:56:02 +0530

    feat: add GDSC route

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/info", (req, res) => {
     res.send("For information related to next session check out GDSC NITS page now")
 })
 
+app.get("/GDSC", (req, res) => {
+    res.send("GDSC NITS is organizing a session on JetPack Compose in the coming weeks")
+})
+
 
 app.listen(5000, () => {
     console.log("Running on port 5000")
